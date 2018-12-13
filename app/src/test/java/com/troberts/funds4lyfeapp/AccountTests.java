eb1d4b495d8884ac5a1ecbddb597e979bbab3fb7
TyrellR7872 <troberts@colgate.edu>
2018-12-13 12:45:26 -0500

    First commit

---
+++ app/src/test/java/com/troberts/funds4lyfeapp/AccountTests.java
@@ -0,0 +1,35 @@
+package com.troberts.funds4lyfeapp;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AccountTests {
+    @Test
+    public void accountContainsAmount(){
+//        UserAccount userAccount = new UserAccount("Tyrell","Roberts",new Date(1997,12,11));
+//        userAccount.createAccount("checking");
+//
+//        assertTrue(userAccount.containsAccount("checking"));
+//        userAccount.deposit("checking",10);
+//        assertTrue(userAccount.displayBalance("checking") == 10);
+//
+//        assertFalse(userAccount.withdraw("checking",20));
+//        assertTrue(userAccount.displayBalance("checking") == 10);
+//        assertTrue(userAccount.total==10);
+//
+//        userAccount.createAccount("savings");
+//        userAccount.deposit("savings",20);
+//        assertTrue(userAccount.total==30);
+
+
+
+    }
+}
